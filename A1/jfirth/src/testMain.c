352f8f4b842057f68ccbda944ac97f26ac4a1c83
Jackson <Jackson@Jacksons-MacBook-Pro.local>
2018-01-17 20:58:30 -0500

    no msg

--- A1/jfirth/src/testMain.c
+++ A1/jfirth/src/testMain.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "GEDCOMparser.h"
+//#include "GEDCOMparser.h"
 
 //*********************
 void deleteField(void* toBeDeleted) {
